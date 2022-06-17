4407d4d2b285b2cc1b5a46a71b75ea7f0d5997a0
Artiom_Sokolov <sokolova-net@mail.ru>
2022-06-17 23:17:02 +0300

    create EmptyCart component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -2,10 +2,24 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import CartItem from '../components/CartItem';
+import { clearItems } from '../redux/slices/cartSlice';
+import CartEmpty from '../components/CartEmpty';
 
 const Cart = () => {
     const dispatch = useDispatch();
-    const items = useSelector((state) => state.cart.items);
+    const { totalPrice, items } = useSelector((state) => state.cart);
+
+    const totalCount = items.reduce((sum, item) => sum + item.count, 0);
+
+    const onClickClear = () => {
+        if (window.confirm('Очистить корзину?')) {
+            dispatch(clearItems());
+        }
+    };
+
+    if (!totalPrice) {
+        return <CartEmpty/>;
+    }
 
     return (
         <div className="conteiner container--cart">
@@ -29,7 +43,9 @@ const Cart = () => {
                                 stroke="white" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                         Корзина</h2>
-                    <div className="cart__clear">
+                    <div
+                        onClick={onClickClear}
+                        className="cart__clear">
                         <svg
                             width="20"
                             height="20"
@@ -57,8 +73,8 @@ const Cart = () => {
                 </div>
                 <div className="cart__bottom">
                     <div className="cart__bottom-details">
-                        <span> Всего пицц: <b>3 шт.</b> </span>
-                        <span> Сумма заказа: <b>900 ₽</b> </span>
+                        <span> Всего пицц: <b>{totalCount} шт.</b> </span>
+                        <span> Сумма заказа: <b>{totalPrice} ₽</b> </span>
                     </div>
                     <div className="cart__bottom-buttons">
                         <Link to="/" className="button button--outline button--add go-back-btn">
--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -37,6 +37,7 @@ export const cartSlice = createSlice({
         },
         clearItems(state, action) {
             state.items = [];
+            state.totalPrice = 0;
         },
     },
 })
